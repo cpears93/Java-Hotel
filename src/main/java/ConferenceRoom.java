6ca1779040698dab8b12ef93165cdcde877e1521
CrugBarat <craigtbarratt@gmail.com>
2020-05-27 12:49:21 +0100

    adds constructor to ConferenceRoom
    Signed-off-by: CrugBarat <craigtbarratt@gmail.com>

--- src/main/java/ConferenceRoom.java
+++ src/main/java/ConferenceRoom.java
@@ -1,2 +1,15 @@
+import java.util.ArrayList;
+
 public class ConferenceRoom {
+
+    private int capacity;
+    private String name;
+    private ArrayList<Guest> collectionOfGuests;
+
+    public ConferenceRoom(int capacity, String name) {
+        this.capacity = capacity;
+        this.collectionOfGuests = new ArrayList<Guest>();
+        this.name = name;
+    }
+
 }
