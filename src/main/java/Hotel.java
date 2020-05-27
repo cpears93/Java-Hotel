f8d5116ec0d9df2b8acef4b7ce891f36941cfac4
CrugBarat <craigtbarratt@gmail.com>
2020-05-27 13:21:18 +0100

    adds getCollectionSize and tests - PASSED
    Signed-off-by: CrugBarat <craigtbarratt@gmail.com>

--- src/main/java/Hotel.java
+++ src/main/java/Hotel.java
@@ -1,2 +1,15 @@
+import java.util.ArrayList;
+
 public class Hotel {
+
+    private ArrayList<Bedroom> collectionOfRooms;
+
+    public Hotel() {
+        this.collectionOfRooms = new ArrayList<Bedroom>();
+    }
+
+
+    public int getCollectionSize() {
+        return this.collectionOfRooms.size();
+    }
 }
--- src/test/java/HotelTest.java
+++ src/test/java/HotelTest.java
@@ -1,4 +1,7 @@
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class HotelTest {
 
@@ -6,7 +9,12 @@ public class HotelTest {
 
     @Before
     public void setUp(){
+        hotel = new Hotel();
+    }
 
+    @Test
+    public void canGetRoomCollectionSizeStartsAtZero() {
+        assertEquals(0,hotel.getCollectionSize());
     }
 
 }
